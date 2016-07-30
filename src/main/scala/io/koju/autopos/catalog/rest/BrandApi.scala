b1b0e87d06bf59ff8591a674488e6780bff8acbd
Kapil Koju <kplx2.0@gmail.com>
2016-07-30 12:04:18 +0545

    Upgrade to Hibernate 5.2.1.Final
    Closes #7

--- src/main/scala/io/koju/autopos/catalog/rest/BrandApi.scala
+++ src/main/scala/io/koju/autopos/catalog/rest/BrandApi.scala
@@ -1,15 +1,14 @@
 package io.koju.autopos.catalog.rest
 
 
-import io.koju.autopos.catalog.domain.{Brand, QBrand}
+import io.koju.autopos.catalog.domain.Brand
 import io.koju.autopos.catalog.service.BrandRepo
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.{HttpStatus, MediaType, ResponseEntity}
-import org.springframework.stereotype.Controller
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RequestMethod.{GET, POST}
-import org.springframework.web.bind.annotation.{PathVariable, RequestBody, RequestMapping}
+import org.springframework.web.bind.annotation._
 
-@Controller
+@RestController
 @RequestMapping(value = Array("/api/brands"),
   consumes = Array(MediaType.APPLICATION_JSON_VALUE),
   produces = Array(MediaType.APPLICATION_JSON_VALUE)
@@ -17,10 +16,8 @@ import org.springframework.web.bind.annotation.{PathVariable, RequestBody, Reque
 class BrandApi @Autowired()(private val brandRepository: BrandRepo) {
 
   @RequestMapping(value = Array("/{id}"), method = Array(GET))
-  def getBrand(@PathVariable("id") id: Long) = {
-    val brand = brandRepository.findOne(id)
-    new ResponseEntity[Brand](brand, HttpStatus.OK)
-  }
+  def getBrand(@PathVariable("id") id: Long) =
+    brandRepository.findOne(id)
 
   @RequestMapping(method = Array(POST))
   def saveBrand(@RequestBody brand: Brand) =
