e662df6fb875ad78c9b9f3cb90a7c0b5159eaf78
Kapil Koju <kplx2.0@gmail.com>
2016-07-31 20:56:18 +0545

    Combine navbar and toolbar

--- webapp/src/client/app/shared/navbar/navbar.component.ts
+++ webapp/src/client/app/shared/navbar/navbar.component.ts
@@ -1,14 +1,11 @@
-import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
 
-/**
- * This class represents the navigation bar component.
- */
 @Component({
-  moduleId: module.id,
-  selector: 'sd-navbar',
-  templateUrl: 'navbar.component.html',
-  styleUrls: ['navbar.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+    moduleId: module.id,
+    selector: 'apos-navbar',
+    templateUrl: 'navbar.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+}
