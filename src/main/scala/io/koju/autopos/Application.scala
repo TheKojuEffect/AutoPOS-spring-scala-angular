d471a073eeaba2beba80dcfef1de09b08f345317
Kapil Koju <kplx2.0@gmail.com>
2016-08-08 08:30:37 +0545

    Add Cors Filter

--- src/main/scala/io/koju/autopos/Application.scala
+++ src/main/scala/io/koju/autopos/Application.scala
@@ -2,7 +2,11 @@ package io.koju.autopos
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.web.cors.{CorsConfiguration, UrlBasedCorsConfigurationSource}
+import org.springframework.web.filter.CorsFilter
 
 object Application extends App {
   SpringApplication.run(classOf[Application])
@@ -11,4 +15,19 @@ object Application extends App {
 
 @EnableJpaAuditing
 @SpringBootApplication
-class Application
+class Application {
+
+  @Bean
+  def corsFilter(): FilterRegistrationBean = {
+    val source = new UrlBasedCorsConfigurationSource
+    val config = new CorsConfiguration
+    config.setAllowCredentials(true)
+    config.addAllowedOrigin("http://localhost:5555")
+    config.addAllowedHeader("*")
+    config.addAllowedMethod("*")
+    source.registerCorsConfiguration("/**", config)
+    val bean = new FilterRegistrationBean(new CorsFilter(source))
+    bean.setOrder(0)
+    bean
+  }
+}
