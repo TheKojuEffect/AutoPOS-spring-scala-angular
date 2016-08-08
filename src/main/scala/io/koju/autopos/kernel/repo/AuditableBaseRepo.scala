2a75badb746df935e3afcc50d7587d326b499a48
Kapil Koju <kplx2.0@gmail.com>
2016-08-08 14:22:06 +0545

    Remove querydsl configuration

--- src/main/scala/io/koju/autopos/kernel/repo/AuditableBaseRepo.scala
+++ src/main/scala/io/koju/autopos/kernel/repo/AuditableBaseRepo.scala
@@ -4,12 +4,10 @@ import java.lang.Long
 
 import io.koju.autopos.kernel.domain.AuditableBaseEntity
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.querydsl.QueryDslPredicateExecutor
 import org.springframework.data.repository.NoRepositoryBean
 
 @NoRepositoryBean
 trait AuditableBaseRepo[T <: AuditableBaseEntity]
-  extends JpaRepository[T, Long]
-    with QueryDslPredicateExecutor[T] {
+  extends JpaRepository[T, Long] {
 
 }
