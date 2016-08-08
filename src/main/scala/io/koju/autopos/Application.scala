45d1900640f77182e3ede1ff62faa86431401e9f
Kapil Koju <kplx2.0@gmail.com>
2016-08-08 14:22:41 +0545

    Configure CORS with web configurer

--- src/main/scala/io/koju/autopos/Application.scala
+++ src/main/scala/io/koju/autopos/Application.scala
@@ -2,11 +2,9 @@ package io.koju.autopos
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.web.cors.{CorsConfiguration, UrlBasedCorsConfigurationSource}
-import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.config.annotation.{CorsRegistry, WebMvcConfigurer, WebMvcConfigurerAdapter}
 
 object Application extends App {
   SpringApplication.run(classOf[Application])
@@ -18,16 +16,11 @@ object Application extends App {
 class Application {
 
   @Bean
-  def corsFilter(): FilterRegistrationBean = {
-    val source = new UrlBasedCorsConfigurationSource
-    val config = new CorsConfiguration
-    config.setAllowCredentials(true)
-    config.addAllowedOrigin("http://localhost:5555")
-    config.addAllowedHeader("*")
-    config.addAllowedMethod("*")
-    source.registerCorsConfiguration("/**", config)
-    val bean = new FilterRegistrationBean(new CorsFilter(source))
-    bean.setOrder(0)
-    bean
+  def corsConfigurer(): WebMvcConfigurer = {
+    new WebMvcConfigurerAdapter() {
+      override def addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+      }
+    }
   }
 }
