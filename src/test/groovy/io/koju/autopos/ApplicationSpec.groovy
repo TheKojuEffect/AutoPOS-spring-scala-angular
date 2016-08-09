9e943afa1c50c1d6b8e0077a40df6fd763b71463
Kapil Koju <kplx2.0@gmail.com>
2016-08-09 23:07:53 +0545

    Add context != null assert

--- src/test/groovy/io/koju/autopos/ApplicationSpec.groovy
+++ src/test/groovy/io/koju/autopos/ApplicationSpec.groovy
@@ -1,6 +1,8 @@
 package io.koju.autopos
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
@@ -8,7 +10,11 @@ import spock.lang.Specification
 @ContextConfiguration
 class ApplicationSpec extends Specification {
 
+    @Autowired
+    ApplicationContext context
+
     def "context loads"() {
-        expect: true
+        expect:
+        context != null
     }
 }
